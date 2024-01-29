dfc5b0e903532b6876a7eab0e406af75aebcd54f
jordan937 <jasselm@siue.edu>
2024-01-29 14:16:09 -0600

    adding file

--- printBackwards.py
+++ printBackwards.py
@@ -2,7 +2,7 @@ def printBackwards (userWord):
     return userWord[::-1]
 
 
-userWord = input("Yo yo, enter a word that will output backwards: ")
+userWord = input("Yo yo, enter a word that will: ")
 wordBackwards = printBackwards(userWord)
 
 print(wordBackwards)
